75f84df5e8bce2a71ef6127fa1a6b8437514ceea
lcolladotor <lcolladotor@gmail.com>
2024-04-22 12:14:26 -0400

    No longer need this

--- spatialLIBD_demo/deploy.R
+++ spatialLIBD_demo/deploy.R
@@ -2,12 +2,12 @@ library('rsconnect')
 
 ## Or you can go to your shinyapps.io account and copy this
 ## Here we do this to keep our information hidden.
-load(".deploy_info.Rdata")
-rsconnect::setAccountInfo(
-    name = deploy_info$name,
-    token = deploy_info$token,
-    secret = deploy_info$secret
-)
+# load(".deploy_info.Rdata")
+# rsconnect::setAccountInfo(
+#     name = deploy_info$name,
+#     token = deploy_info$token,
+#     secret = deploy_info$secret
+# )
 
 ## You need this to enable shinyapps to install Bioconductor packages
 options(repos = BiocManager::repositories())
